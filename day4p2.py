91dff1fc236d80d8eaa05c6412dbf82e2c288b1e
Miguel Buzani <miguel.buzani@construplan.com.mx>
2022-08-19 10:32:15 -0700

    day4p2 done?

--- day4p2.py
+++ day4p2.py
@@ -1,3 +1,4 @@
+import math as Math
 import sys
 from locale import atoi
 import time
@@ -13,35 +14,44 @@ currentNums = []
 finalNum = 0
 
 winnerNums = []
-numboards = 0
-print((len(lines) - 1))
+winnerBoards = []
+winnerRows = []
+winnerCols = [[0] * 5] * len(lines)
+# print(winnerCols)
+# numboards = 0
+numBoards = (len(lines) - 1) / 6
+# print(nums)
 
 for i in nums:
     currentNums.append(i.strip())
+
+    if (len(winnerBoards) == 3):
+        break
 
     boardNum = 0
     unmarkedSum = 0
     flag = False
     boardY = []
-    numboards = 0
+    # numboards = 0
 
     for j in range(2, len(lines)):
+
         scoreX = 0
         scoreY = {}
         #print(lines[j])
 
         if (lines[j] == "\n"):
 
-            if (flag):
+            # if (flag):
 
-                break
+            #     break
             scoreX = 0
             scoreY = {}
             unmarkedSum = 0
             #print(boardY)
             boardY = []
             #print(lines[j])
-            numboards += 1
+            # numboards += 1
 
         else:
             rowNums = lines[j].split(' ')
@@ -51,6 +61,26 @@ for i in nums:
                 num = rowNums[k].strip()
                 if (num in currentNums):
                     scoreX += 1
+                    if (scoreX == 5):
+                        if (j not in winnerRows):
+                            flag = True
+                            #winnerNums.append(finalNum)
+                            winnerRows.append(j)
+                            finalNum = atoi(i)
+                            tempJ = j
+                            temp2J = j - 2
+                            # print("Board: %s" % (numboards / 6))
+                            # print(j)
+                            asdf = ((temp2J) / 18)
+                            # print((int)(asdf * numBoards))
+                            winnerBoards.append((int)(asdf * numBoards))
+                            # print(i, j, (int)(asdf * numBoards), currentNums)
+                            # print([(int)(asdf * numBoards), i, j + 1])
+                            # print(asdf)
+                            # print((((j) / 18) * numBoards).toString("#"))
+                            # print("Winner: %s" % i)
+                    # print(scoreY, j, k)
+
                     scoreY[k] = 1
 
                 else:
@@ -75,26 +105,32 @@ for i in nums:
 
                 for ii in range(0, 5):
                     # print(totals[i])
+                    # print(j, ii)
+
                     if totals[ii] == 5:
-                        flag = True
-                        finalNum = atoi(i)
-                        tempJ = j
-                        print("Board: %s" % (numboards / 6))
-                        print(i)
-                # print(totals)
+                        if (winnerCols[j][ii] == 0):
+                            # print(totals)
+                            flag = True
+                            finalNum = atoi(i)
+                            tempJ = j
+                            temp2J = j - 2
+                            # print("Board: %s" % (numboards / 6))
+                            # print(j)
+                            asdf = ((temp2J) / 18)
+                            winnerCols[j][ii] = 1
+                            # winnerBoards.append([(int)(asdf * numBoards), i])
+                            winnerBoards.append((int)(asdf * numBoards))
+                            # print(i, j, ii, (int)(asdf * numBoards),
+                            #       currentNums)
+                            # print("Board: %s" % (numboards / 6))
+                            # print(i)
+                    else:
+                        winnerCols[j][ii] = 0
 
                 boardY.append(totals)
+            # print(totals)
 
             #print(boardY)
-
-            if (scoreX == 5):
-                flag = True
-                #winnerNums.append(finalNum)
-                #print(i)
-                finalNum = atoi(i)
-                tempJ = j
-                print("Board: %s" % (numboards / 6))
-                print(i)
 
     # if (flag):
     #     break
@@ -109,3 +145,4 @@ print(finalNum * unmarkedSum)
 print(finalNum)
 print(unmarkedSum)
 print(winnerNums)
+print(winnerBoards[len(winnerBoards) - 1])
